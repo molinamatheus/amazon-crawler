9ed7fef0d65dbf04c4ad16832072cd1aef8beb9c
Matheus Molina Matsumoto Junior <99224273+molinamatheus@users.noreply.github.com>
2022-02-13 15:55:04 -0300

    Finish basic crawler
    without database

--- version1.0.py
+++ version1.0.py
@@ -6,7 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.webdriver import WebDriver
 
-path = 'https://www.amazon.com/s?bbn=16225009011&rh=n%3A2811119011&dc&qid=1644500557&rnid=16225009011&ref=lp_16225009011_nr_n_3'
+base = 'https://www.amazon.com'
+path = '/s?bbn=16225009011&rh=n%3A2811119011&dc&qid=1644500557&rnid=16225009011&ref=lp_16225009011_nr_n_3'
 # this is the variable reffering to the website
 
 chrome_options = webdriver.ChromeOptions()
@@ -16,11 +17,14 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 # Selenium WebDriver is an automated testing framework used for website validation
 # The ChromeOptions class was introduced in the latest/updated version of Selenium.
 # It is useful to make changes in the Chrome browser
-service = Service('.\chromedriver')
+service = Service('chromedriver')
 
-browser: WebDriver = webdriver.Chrome('.\chromedriver')
+browser: WebDriver = webdriver.Chrome('chromedriver')
+
+page = 0
 while path is not None:
-    browser.get(path)
+    print('page ', page)
+    browser.get(base + path)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
     # print('soup', soup)
     # this moment the script open dev function in browser (type inspect)
@@ -29,7 +33,6 @@ while path is not None:
     # <li> (Lista dos Itens de um elemento HTML) é usado para representar um item que faz parte de uma lista.
     # Este item deve estar contido em uma lista desordenada ( <ul> )
     job_elements = soup.select(_searchresult)
-    print(len(job_elements))
     # len return the number of elements in any list
     _title = 'div[class="a-section a-spacing-none a-spacing-top-small s-title-instructions-style"] > h2'
     for x in job_elements:
@@ -39,30 +42,36 @@ while path is not None:
         else:
             t = _t[0]
             title = t.get_text()
-            print('title: ', title)
 
             _pricelist = x.select('span[class="a-price"] > span[class="a-offscreen"]')
-            print('_pricelist:', _pricelist)
             valuePrice = None
 
             for _price in _pricelist:
                 # The for loop allows you to execute a block of code over and over again until a condition is true.
                 # The in operator checks if the operand to its left is contained in the list to its right.
                 valuePriceText = _price.get_text()
-                print('valuePriceText', valuePriceText)
-                valuePricePresent = float(valuePriceText.replace('$', ''))
+                valuePricePresent = float(valuePriceText.replace('$', '').replace(',', ''))
                 if valuePrice == None:
                     valuePrice = valuePricePresent
                 else:
                     if valuePricePresent < valuePrice:
                         valuePrice = valuePricePresent
             price = str(valuePrice)
-            print('iten:' + title)
-            print('price:' + price)
+            print('-------------------------------')
+            print('item: ' + title)
+            print('price: ' + price)
             # return all results in string (text)
 
-    _nextpg = soup.select(
-        '#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(40) > div > div > span > a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator')
+    _nextpg = soup.select('a[class="s-pagination-item s-pagination-next s-pagination-button s-pagination-separator"]')
     print(_nextpg)
-    path = _nextpg[0].get('href')
+    if (_nextpg is None or len(_nextpg) == 0):
+        path = None
+    else:
+        path = _nextpg[0].get('href')
+        print('######')
+        print('new path:' + path)
+        page = page + 1
     # this script is responsible to introduce next page in browser
+
+print('end')
+browser.close()
